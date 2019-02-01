d4ecc9cf2e7f9ad636de6da1718d71149c1e2549
Yeqing Liu <chingliu2@gmail.com>
2019-02-01 11:56:49 -0500

    added run server and close server functionalities

--- server.js
+++ server.js
@@ -14,6 +14,42 @@ app.get('/api/*', (req, res) => {
     res.json({ok: true});
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+let server;
 
-module.exports = {app};
+function runServer(databaseUrl, port = PORT){
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if(err){
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Listening on port ${port}`);
+                resolve();
+            })
+            .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
+        });
+    });
+}
+
+function closeServer(){
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if(err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+if (require.main === module){
+    runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = {app, runServer, closeServer};
